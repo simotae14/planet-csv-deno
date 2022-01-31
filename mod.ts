4c1e589d511da3266588a1b9b36f4ba73b2a1c85
Simone <startae14@gmail.com>
2022-01-31 16:58:30 +0100

    06: define some Types for the planets and filter to find the habitable planets

--- mod.ts
+++ mod.ts
@@ -2,6 +2,10 @@ import { join } from "https://deno.land/std/path/mod.ts";
 import { BufReader } from "https://deno.land/std@0.123.0/io/buffer.ts";
 import { parse } from "https://deno.land/std/encoding/csv.ts";
 
+interface Planet {
+    [ key: string ] : string
+}
+
 async function loadPlanetsData() {
     const path = join(".", "kepler_exoplanets_nasa.csv");
     const file = await Deno.open(path);
@@ -12,10 +16,19 @@ async function loadPlanetsData() {
         comment: "#",// how to identify the comments
       });
     Deno.close(file.rid);
-    console.log("result", result);
+    const planets = (result as Array<Planet>).filter((planet) => {
+        const planetaryRadius = Number(planet["koi_prad"]);
+        const stellarMass = Number(planet["koi_smass"]);
+        const stellarRadius = Number(planet["koi_srad"]);
+        return planet["koi_disposition"] === "CONFIRMED"
+            && planetaryRadius > 0.5 && planetaryRadius < 1.5
+            && stellarMass > 0.78 && stellarMass < 1.04
+            && stellarRadius > 0.99 && stellarRadius < 1.01;
+    })
+    return planets;
 }
 
-await loadPlanetsData();
-
+const newEarths = await loadPlanetsData();
+console.log(`${newEarths.length} habitable planets found!`);
 // run with
 // deno run --allow-read mod.ts
