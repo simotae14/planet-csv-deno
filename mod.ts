728e58788314ba8e99ae37bfe7fdf59fc6a7e779
Simone <startae14@gmail.com>
2022-01-31 11:48:57 +0100

    01: how to read a File text, first example
    and how to launch this function

--- mod.ts
+++ mod.ts
@@ -1,6 +1,9 @@
-function readFile() {
+async function readFile() {
     const data = await Deno.readTextFile("heeello.txt");
     console.log(data);
 }
 
 readFile();
+
+// run with
+// deno run --allow-read mod.ts
