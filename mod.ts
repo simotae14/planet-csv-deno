546722be606e1a95f07888e5e4813ab1a1f79d9e
Simone <startae14@gmail.com>
2022-01-31 17:31:33 +0100

    07: filter the data to show using lodash and its pick method

--- mod.ts
+++ mod.ts
@@ -2,6 +2,8 @@ import { join } from "https://deno.land/std/path/mod.ts";
 import { BufReader } from "https://deno.land/std@0.123.0/io/buffer.ts";
 import { parse } from "https://deno.land/std/encoding/csv.ts";
 
+import * as _ from "https://raw.githubusercontent.com/lodash/lodash/4.17.21-es/lodash.js";
+
 interface Planet {
     [ key: string ] : string
 }
@@ -25,10 +27,23 @@ async function loadPlanetsData() {
             && stellarMass > 0.78 && stellarMass < 1.04
             && stellarRadius > 0.99 && stellarRadius < 1.01;
     })
-    return planets;
+    return planets.map((planet) => {
+        return _.pick(planet, [
+            "koi_prad",
+            "koi_smass",
+            "koi_srad",
+            "kepler_name",
+            "koi_count",
+            "koi_steff"
+        ]);
+    });
 }
 
 const newEarths = await loadPlanetsData();
+// log infos about habitable planets
+for (const planet of newEarths) {
+    console.log(planet);
+}
 console.log(`${newEarths.length} habitable planets found!`);
 // run with
 // deno run --allow-read mod.ts
