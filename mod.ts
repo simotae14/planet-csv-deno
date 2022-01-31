38d72b49b269968753fb3cccc9edd924df40f0d2
Simone <startae14@gmail.com>
2022-01-31 15:47:54 +0100

    05: Read and parse a CSV file in Deno with BufReader and parse standard library

--- mod.ts
+++ mod.ts
@@ -1,12 +1,21 @@
 import { join } from "https://deno.land/std/path/mod.ts";
+import { BufReader } from "https://deno.land/std@0.123.0/io/buffer.ts";
+import { parse } from "https://deno.land/std/encoding/csv.ts";
 
-async function readFile() {
-    const path = join("text_files", "heeello.txt");
-    const data = await Deno.readTextFile(path);
-    console.log(data);
+async function loadPlanetsData() {
+    const path = join(".", "kepler_exoplanets_nasa.csv");
+    const file = await Deno.open(path);
+    // create a new BufReader
+    const bufReader = new BufReader(file);
+    const result = await parse(bufReader, {
+        skipFirstRow: true,
+        comment: "#",// how to identify the comments
+      });
+    Deno.close(file.rid);
+    console.log("result", result);
 }
 
-await readFile();
+await loadPlanetsData();
 
 // run with
 // deno run --allow-read mod.ts
